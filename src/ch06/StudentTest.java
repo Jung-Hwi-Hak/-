034d9ca417f30742d1a88981ef7c6aa929563750
Jung HwiHak <gnlgkrdl@naver.com>
2022-12-23 19:39:26 +0900

    인스턴스, 생성자

---
+++ src/ch06/StudentTest.java
@@ -0,0 +1,16 @@
+package ch06;
+
+public class StudentTest {
+
+	public static void main(String[] args) {
+		
+		Student studentLee = new Student(4488, "Lee", 1);
+		
+//		studentLee.studentNumber = 4488;
+//		studentLee.studentName = "Lee";
+//		studentLee.grade = 1;
+		
+		System.out.println(studentLee.showStudentInfo()); 
+	}
+
+}
